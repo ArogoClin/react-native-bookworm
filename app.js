d87e26a1cadd06770f9aadec37d60dd21c337f23
Arogo <clintonomosh254@gmail.com>
2025-11-18 11:34:11 +0300

    Increase payload limit to handle base64 images

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+
+// Increase payload limit to handle base64 images
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Define your routes here
 app.use('/api/auth', authRoutes);
